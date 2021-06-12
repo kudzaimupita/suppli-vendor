3da49d85344ed5aadf9b807d1e4159024feb6c72
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-06-12 18:19:49 +0200

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -27,129 +27,129 @@ import { getCatergories } from "../../actions/catergories";
 import { loadMyPlug } from "../../actions/auth";
 // reactstrap components
 import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  Collapse,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
-  DropdownToggle,
-  FormGroup,
-  Form,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  Media,
-  NavbarBrand,
-  Navbar,
-  NavItem,
-  NavLink,
-  Nav,
-  Progress,
-  Table,
-  Container,
-  Row,
-  Col,
+	Button,
+	Card,
+	CardHeader,
+	CardBody,
+	CardTitle,
+	Collapse,
+	DropdownMenu,
+	DropdownItem,
+	UncontrolledDropdown,
+	DropdownToggle,
+	FormGroup,
+	Form,
+	Input,
+	InputGroupAddon,
+	InputGroupText,
+	InputGroup,
+	Media,
+	NavbarBrand,
+	Navbar,
+	NavItem,
+	NavLink,
+	Nav,
+	Progress,
+	Table,
+	Container,
+	Row,
+	Col,
 } from "reactstrap";
 
 var ps;
 
 class Sidebar extends React.Component {
-  componentDidMount() {
-    this.props.loadMyPlug();
-  }
-  state = {
-    collapseOpen: false,
-  };
-  constructor(props) {
-    super(props);
-    this.activeRoute.bind(this);
-  }
-  // verifies if routeName is the one active (in browser input)
-  activeRoute(routeName) {
-    return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
-  }
-  // toggles collapse between opened and closed (true/false)
-  toggleCollapse = () => {
-    this.setState({
-      collapseOpen: !this.state.collapseOpen,
-    });
-  };
-  // closes the collapse
-  closeCollapse = () => {
-    this.setState({
-      collapseOpen: false,
-    });
-  };
-  // creates the links that appear in the left menu / Sidebar
-  createLinks = (routes) => {
-    return routes.map((prop, key) => {
-      return (
-        <NavItem key={key}>
-          <NavLink
-            to={prop.layout + prop.path}
-            tag={NavLinkRRD}
-            onClick={this.closeCollapse}
-            activeClassName="active"
-          >
-            <i className={prop.icon} />
-            {prop.name}
-          </NavLink>
-        </NavItem>
-      );
-    });
-  };
-  render() {
-    const { bgColor, routes, logo } = this.props;
-    let navbarBrandProps;
-    if (logo && logo.innerLink) {
-      navbarBrandProps = {
-        to: logo.innerLink,
-        tag: Link,
-      };
-    } else if (logo && logo.outterLink) {
-      navbarBrandProps = {
-        href: logo.outterLink,
-        target: "_blank",
-      };
-    }
-    return (
-      <Navbar
-        className="navbar-vertical fixed-left navbar-light bg-white"
-        expand="md"
-        id="sidenav-main"
-      >
-        <Container fluid>
-          {/* Toggler */}
-          <button
-            className="navbar-toggler"
-            type="button"
-            onClick={this.toggleCollapse}
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          {/* Brand */}
+	componentDidMount() {
+		this.props.loadMyPlug();
+	}
+	state = {
+		collapseOpen: false,
+	};
+	constructor(props) {
+		super(props);
+		this.activeRoute.bind(this);
+	}
+	// verifies if routeName is the one active (in browser input)
+	activeRoute(routeName) {
+		return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+	}
+	// toggles collapse between opened and closed (true/false)
+	toggleCollapse = () => {
+		this.setState({
+			collapseOpen: !this.state.collapseOpen,
+		});
+	};
+	// closes the collapse
+	closeCollapse = () => {
+		this.setState({
+			collapseOpen: false,
+		});
+	};
+	// creates the links that appear in the left menu / Sidebar
+	createLinks = (routes) => {
+		return routes.map((prop, key) => {
+			return (
+				<NavItem key={key}>
+					<NavLink
+						to={prop.layout + prop.path}
+						tag={NavLinkRRD}
+						onClick={this.closeCollapse}
+						activeClassName="active"
+					>
+						<i className={prop.icon} />
+						{prop.name}
+					</NavLink>
+				</NavItem>
+			);
+		});
+	};
+	render() {
+		const { bgColor, routes, logo } = this.props;
+		let navbarBrandProps;
+		if (logo && logo.innerLink) {
+			navbarBrandProps = {
+				to: logo.innerLink,
+				tag: Link,
+			};
+		} else if (logo && logo.outterLink) {
+			navbarBrandProps = {
+				href: logo.outterLink,
+				target: "_blank",
+			};
+		}
+		return (
+			<Navbar
+				className="navbar-vertical fixed-left navbar-light bg-white"
+				expand="md"
+				id="sidenav-main"
+			>
+				<Container fluid>
+					{/* Toggler */}
+					<button
+						className="navbar-toggler"
+						type="button"
+						onClick={this.toggleCollapse}
+					>
+						<span className="navbar-toggler-icon" />
+					</button>
+					{/* Brand */}
 
-          <NavbarBrand className="pt-0">
-            {/* <img
+					<NavbarBrand className="pt-0">
+						{/* <img
                 alt={logo.imgAlt}
                 className="navbar-brand-img"
                 src={logo.imgSrc}
               /> */}
-            {this.props.myPlug.name}
-          </NavbarBrand>
+						{this.props.myPlug.name}
+					</NavbarBrand>
 
-          {/* User */}
-          <Nav className="align-items-center d-md-none">
-            <UncontrolledDropdown nav>
-              {/* <DropdownToggle nav className="nav-link-icon">
+					{/* User */}
+					<Nav className="align-items-center d-md-none">
+						<UncontrolledDropdown nav>
+							{/* <DropdownToggle nav className="nav-link-icon">
                 <i className="ni ni-bell-55" />
               </DropdownToggle> */}
-              {/* <DropdownMenu
+							{/* <DropdownMenu
                 aria-labelledby="navbar-default_dropdown_1"
                 className="dropdown-menu-arrow"
                 right
@@ -159,120 +159,92 @@ class Sidebar extends React.Component {
                 <DropdownItem divider />
                 <DropdownItem>Something else here</DropdownItem>
               </DropdownMenu> */}
-            </UncontrolledDropdown>
-           < NavUserDropdown />
-          </Nav>
-          {/* Collapse */}
-          <Collapse navbar isOpen={this.state.collapseOpen}>
-            {/* Collapse header */}
-            <div className="navbar-collapse-header d-md-none">
-              <Row>
-                {logo ? (
-                  <Col className="collapse-brand" xs="6">
-                    {logo.innerLink ? (
-                      <Link to={logo.innerLink}>
-                        <img alt={logo.imgAlt} src={logo.imgSrc} />
-                      </Link>
-                    ) : (
-                      <a href={logo.outterLink}>
-                        <img alt={logo.imgAlt} src={logo.imgSrc} />
-                      </a>
-                    )}
-                  </Col>
-                ) : null}
-                <Col className="collapse-close" xs="6">
-                  <button
-                    className="navbar-toggler"
-                    type="button"
-                    onClick={this.toggleCollapse}
-                  >
-                    <span />
-                    <span />
-                  </button>
-                </Col>
-              </Row>
-            </div>
-            {/* Form */}
-        
-            {/* Navigation */}
-            <Nav navbar>{this.createLinks(routes)}</Nav>
-            {/* Divider */}
-            <hr className="my-3" />
-            {/* Heading */}
-            <h6 className="navbar-heading text-muted">Help</h6>
-            {/* Navigation */}
-            <Nav className="mb-md-3" navbar>
-              <NavItem>
-                <NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/overview?ref=adr-admin-sidebar">
-                  <i className="ni ni-spaceship" />
-                  FAQ
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/colors?ref=adr-admin-sidebar">
-                  <i className="ni ni-palette" />
-                  Contact Us
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/alerts?ref=adr-admin-sidebar">
-                  <i className="ni ni-ui-04" />
-                  About
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <hr className="my-3" />
-            <Nav className="mb-md-3" navbar>
-              <NavItem className="active-pro active">
-                <NavLink href="https://www.creative-tim.com/product/argon-dashboard-pro-react?ref=adr-admin-sidebar">
-                  <i className="ni ni-spaceship" />
-                  EXIT DASHBOARD
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
+						</UncontrolledDropdown>
+						<NavUserDropdown />
+					</Nav>
+					{/* Collapse */}
+					<Collapse navbar isOpen={this.state.collapseOpen}>
+						{/* Collapse header */}
+						<div className="navbar-collapse-header d-md-none">
+							<Row>
+								{logo ? (
+									<Col className="collapse-brand" xs="6">
+										{logo.innerLink ? (
+											<Link to={logo.innerLink}>
+												<img alt={logo.imgAlt} src={logo.imgSrc} />
+											</Link>
+										) : (
+											<a href={logo.outterLink}>
+												<img alt={logo.imgAlt} src={logo.imgSrc} />
+											</a>
+										)}
+									</Col>
+								) : null}
+								<Col className="collapse-close" xs="6">
+									<button
+										className="navbar-toggler"
+										type="button"
+										onClick={this.toggleCollapse}
+									>
+										<span />
+										<span />
+									</button>
+								</Col>
+							</Row>
+						</div>
+
+						<hr className="my-3" />
+						<Nav className="mb-md-3" navbar>
+							<NavItem className="active-pro active">
+								<NavLink onClick={() => this.props.logout()}>
+									<i className="ni ni-spaceship" />
+									EXIT DASHBOARD
+								</NavLink>
+							</NavItem>
+						</Nav>
+					</Collapse>
+				</Container>
+			</Navbar>
+		);
+	}
 }
 
 Sidebar.defaultProps = {
-  routes: [{}],
+	routes: [{}],
 };
 
 Sidebar.propTypes = {
-  // links that will be displayed inside the component
-  routes: PropTypes.arrayOf(PropTypes.object),
-  logo: PropTypes.shape({
-    // innerLink is for links that will direct the user within the app
-    // it will be rendered as <Link to="...">...</Link> tag
-    innerLink: PropTypes.string,
-    // outterLink is for links that will direct the user outside the app
-    // it will be rendered as simple <a href="...">...</a> tag
-    outterLink: PropTypes.string,
-    // the image src of the logo
-    imgSrc: PropTypes.string.isRequired,
-    // the alt for the img
-    imgAlt: PropTypes.string.isRequired,
-  }),
+	// links that will be displayed inside the component
+	routes: PropTypes.arrayOf(PropTypes.object),
+	logo: PropTypes.shape({
+		// innerLink is for links that will direct the user within the app
+		// it will be rendered as <Link to="...">...</Link> tag
+		innerLink: PropTypes.string,
+		// outterLink is for links that will direct the user outside the app
+		// it will be rendered as simple <a href="...">...</a> tag
+		outterLink: PropTypes.string,
+		// the image src of the logo
+		imgSrc: PropTypes.string.isRequired,
+		// the alt for the img
+		imgAlt: PropTypes.string.isRequired,
+	}),
 };
 
 Sidebar.defaultProps = {
-  catergories: [],
-  myPlug: {},
+	catergories: [],
+	myPlug: {},
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth,
+	auth: state.auth,
 
-  isAuthenticated: state.auth,
-  cartItems: state.cart.cartItems,
-  myPlug: state.myPlug.plug,
+	isAuthenticated: state.auth,
+	cartItems: state.cart.cartItems,
+	myPlug: state.myPlug.plug,
 });
 
 export default connect(mapStateToProps, {
-  logout,
-  loadMyPlug,
-  getCatergories,
+	logout,
+	loadMyPlug,
+	getCatergories,
 })(Sidebar);
