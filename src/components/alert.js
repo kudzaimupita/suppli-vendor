006bdec5719c72c6dec4a91c86374d05384fa6e3
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-06-18 23:53:24 +0200

    Update alert.js

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,95 +1,30 @@
-import React, { Component } from "react";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, notification, Divider, Space } from 'antd';
 
-import NotificationAlert from "react-notification-alert";
-import { connect } from "react-redux";
-import { SelectAlert } from "../selectors/alertSelector";
 
-class Alert extends React.PureComponent {
-  componentDidUpdate(prevState) {
-    if (prevState.alerts !== this.props.alerts) {
-      this.renderAlert();
-    }
-  }
-  notify = (place, type, message, id) => {
-    var options = {};
-    options = {
-      place: place,
-      message: (
-        <div className="alert-text">
-          <span data-notify="message">
-            {"  "}
-            {message}
-          </span>
-        </div>
-      ),
-      type: type,
-      icon: "ni ni-bell-55",
-      autoDismiss: 3,
+const Alert = () => {
+    const alerts = useSelector((state) => state.alert);
+    const openNotification = (placement, message, type) => {
+        notification[type]({
+            message: `Notification `,
+            description: message,
+            placement,
+        });
     };
-    this.refs.notificationAlert.notificationAlert(options);
-  };
 
-  renderAlert() {
-    {
-      this.props.alerts.map((alert) =>
-        this.notify("tl", alert.alertType, alert.msg, alert.id)
-      );
-    }
-  }
-
-  render() {
     return (
-      <>
-        <div className="rna-wrapper">
-          <NotificationAlert ref="notificationAlert" />
-        </div>
-      </>
-    );
-  }
+        <>
+            {alerts.map((alert) =>
+                openNotification(
+                    'topRight',
+                    alert.msg,
+
+                    alert.alertType
+                )
+            )}
+        </>
+    )
 }
 
-const mapStateToProps = (state) => ({
-  alerts: SelectAlert(state),
-});
-
-export default connect(mapStateToProps)(Alert);
-
-// import React, { Component } from "react";
-// import NotificationAlert from "react-notification-alert";
-// import { connect } from "react-redux";
-
-// var options = {};
-// options = {
-//   place: "tl",
-//   message: (
-//     <div>
-//       <div>ghjknhuhunjk</div>
-//     </div>
-//   ),
-//   type: "danger",
-//   icon: "now-ui-icons ui-1_bell-53",
-//   autoDismiss: 3,
-// };
-
-// class Alert extends Component {
-//   componentDidMount() {
-//     console.log(this.props.alerts);
-//   }
-//   myFunc() {
-//     this.refs.notify.notificationAlert(options);
-//   }
-//   render() {
-//     // console.log(this.props.alerts);
-//     return (
-//       <div>
-//         <NotificationAlert ref="notify" />
-//       </div>
-//     );
-//   }
-// }
-
-// const mapStateToProps = (state) => ({
-//   alerts: state.alert,
-// });
-
-// export default connect(mapStateToProps)(Alert);
+export default Alert
