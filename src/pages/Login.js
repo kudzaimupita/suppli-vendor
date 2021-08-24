8fd0e8d2acf5aff017e5ff335210bb15a17211e9
Kudzai.Mupita <kudzai.mupita@kaleidocode.co.za>
2021-08-24 18:22:44 +0200

    new

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -128,7 +128,7 @@ class Login extends Component {
                                 </div>
                                 <button
                                     type="submit"
-                                    className="my-3 mb-4 submit group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                                    className="my-3  submit group relative w-full flex justify-center py-2 px-4 mb-5 border border-transparent text-sm font-medium rounded-md text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
                                 >
                                     <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                                         <LockClosedIcon className="h-5 w-5 text-gray-500 group-hover:text-grayu-400" aria-hidden="true" />
